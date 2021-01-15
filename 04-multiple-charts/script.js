c8b7a07de8f7b1a714814e6a9c354ca9c47ebae7
kunxin-chor <chorkunxin@yahoo.com>
2021-01-15 06:50:54 +0000

    Added synchronized charts

--- 04-multiple-charts/script.js
+++ 04-multiple-charts/script.js
@@ -1,18 +1,22 @@
-// 1. we define the chart options. 
-const options =  {
+const sightings = [10, 13, 15, 22, 34, 23, 55, 78, 44, 31];
+const temperature = [33, 21, 22, 24, 25, 26, 26, 21, 31, 44];
+
+// Create the line chart
+const lineChartOptions =  {
     chart: {
-        type:'line',
-        height:"100%"
+        type: 'line',
+        height:"100%",
+        width:"100%"
     },
     // each series represents one set of data
     series:[
         {
             name: 'sightings',
-            data:[10, 13, 15, 22, 34, 23, 55, 78, 44, 31]
+            data: sightings
         },
         {
             name: 'temperature',
-            data:[33, 21, 22, 24, 25, 26, 26, 21, 31, 44]
+            data:temperature
         }
     ],
     // what is are the labels along the x-axis (horizontal line)
@@ -23,8 +27,36 @@ const options =  {
 }
 
 // create the chart
-const chart = new ApexCharts(document.querySelector('#chart'), options);
+const lineChart = new ApexCharts(document.querySelector('#lineChart'), lineChartOptions);
 
 // render the chart
-chart.render()
+lineChart.render()
 
+const barChartOptions =  {
+    chart: {
+        type: 'bar',
+        height:"100%"
+    },
+    // each series represents one set of data
+    series:[
+        {
+            name: 'sightings',
+            data: sightings
+        },
+        {
+            name: 'temperature',
+            data:temperature
+        }
+    ],
+    // what is are the labels along the x-axis (horizontal line)
+    xaxis: {
+        categories:['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct']
+    },
+    
+}
+
+// create the chart
+const barChart = new ApexCharts(document.querySelector('#barChart'), barChartOptions);
+
+// render the bar chart
+barChart.render()
