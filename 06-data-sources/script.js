85d15dd61df395893fd4e4b83a8e229be6190c96
kunxin-chor <chorkunxin@yahoo.com>
2021-01-26 07:24:34 +0000

    Added synchronized charts

---
+++ 06-data-sources/script.js
@@ -0,0 +1,30 @@
+
+
+const options =  {
+    chart: {
+        type: 'line',
+        height:"100%"
+    },
+    series:[
+        
+    ],
+    noData: {
+        "text": "Loading..."
+    }
+   
+    
+}
+
+// create the chart
+const chart = new ApexCharts(document.querySelector('#chart'), options);
+
+// render the chart
+chart.render()
+
+window.addEventListener('DOMContentLoaded', async ()=>{
+    let series = await loadData();
+    chart.updateSeries([{
+        'name': 'Sales',
+        'data': series.yearly
+    }])
+})
