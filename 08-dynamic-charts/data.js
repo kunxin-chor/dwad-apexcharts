208e22799421f66e76d21c654f9aa1dd19312a02
kunxin-chor <chorkunxin@yahoo.com>
2021-02-01 03:28:47 +0000

    Added hands-on 9

--- 08-dynamic-charts/data.js
+++ 08-dynamic-charts/data.js
@@ -1,4 +1,60 @@
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
 async function loadData() {
-    const response = await axios.get('https://raw.githubusercontent.com/kunxin-chor/data-files-and-stuff/master/bigger-sales.json');
-    return response.data;
+  const response = await axios.get(
+    "https://raw.githubusercontent.com/kunxin-chor/data-files-and-stuff/master/bigger-sales.json"
+  );
+  return response.data;
+}
+
+function transformData(data, year, country) {
+  // step 1: convert the completed_at property from string to Date data type
+  let transformed = data.map(function(datnum) {
+    return {
+      ...datnum,
+      completed_at: new Date(datnum.completed_at)
+    };
+  });
+  // step 2: filter and keep those records that matches the year and country
+  let filtered = transformed.filter(function(datnum) {
+    return (
+      datnum.completed_at.getFullYear() == year &&
+      (datnum.customer.country.toLowerCase().includes(country.toLowerCase()) ||
+        country == "")
+    );
+  });
+  // step 3: extract out only the amount and the month
+  let earnings = filtered.map(function(datnum) {
+    return {
+      amount: parseInt(datnum.payment.amount),
+      month: datnum.completed_at.getMonth()
+    };
+  });
+
+  // step 4: group by months
+  let groups = groupBy(earnings, "month");
+
+  // step 5: convert into series
+  let series = [];
+  for (let month in groups) {
+    let group = groups[month];
+    series.push({
+      x: monthNames[month],
+      y: group.reduce((acc, datanum) => acc + datanum.amount, 0)
+    });
+  }
+  return series;
 }
--- 08-dynamic-charts/script.js
+++ 08-dynamic-charts/script.js
@@ -1,21 +1,4 @@
-const groupBy = function(data, key) {
-  // `data` is an array of objects, `key` is the key (or property accessor) to group by
-  // reduce runs this anonymous function on each element of `data` (the `item` parameter,
-  // returning the `storage` parameter at the end
-  return data.reduce(function(storage, item) {
-    // get the first instance of the key by which we're grouping
-    var group = item[key];
 
-    // set `storage` for this instance of group to the outer scope (if not empty) or initialize it
-    storage[group] = storage[group] || [];
-
-    // add this item to its group within `storage`
-    storage[group].push(item);
-
-    // return the updated storage to the reduce function, which will then loop through the next
-    return storage;
-  }, {}); // {} is the initial value of the storage
-};
 
 const options = {
   chart: {
@@ -28,68 +11,10 @@ const options = {
   }
 };
 
-const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
+
 
 function updateChart(data, chart, searchTerms) {
-  let transformed = data.map(function(datnum) {
-    return {
-      ...datnum,
-      completed_at: new Date(datnum.completed_at)
-    };
-  });
-  let filtered = transformed.filter(function(datnum) {
-    return (
-      datnum.completed_at.getFullYear() == 2020 &&
-      (datnum.customer.country
-        .toLowerCase()
-        .includes(searchTerms.country.toLowerCase()) ||
-        searchTerms.country == "")
-    );
-  });
-  let earnings = filtered.map(function(datnum) {
-    return {
-      amount: datnum.payment.amount,
-      date: datnum.completed_at
-    };
-  });
-
-  let byMonth = earnings.map(function(datanum) {
-    return {
-      amount: parseInt(datanum.amount),
-      month: datanum.date.getMonth()
-    };
-  });
-  let groups = groupBy(byMonth, "month");
-
-  //   let series = Object.values(groups).map(function(group, month) {
-  //     return {
-  //       x: monthNames[month],
-  //       y: group.reduce((acc, datanum) => acc + datanum.amount, 0)
-  //     };
-  //   });
-  let series = [];
-  for (let month in groups) {
-    let group = groups[month];
-    series.push({
-      x: monthNames[month],
-      y: group.reduce((acc, datanum) => acc + datanum.amount, 0)
-    });
-  }
-  console.log(series);
-
+  let series = transformData(data, searchTerms.year, searchTerms.country)
   chart.updateSeries([
     {
       name: "Sales",
